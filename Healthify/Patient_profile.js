a47588ccf64b3bde2ff31b1185df0d8bf1de2b3d
sameer <sameerdhanawade17@gmail.com>
2017-01-19 00:35:07 +0530

    Added Patient_profile and appointments page

--- Healthify/Patient_profile.js
+++ Healthify/Patient_profile.js
@@ -1,6 +1,19 @@
 var app=angular.module('Profile',[]);
 
 app.controller('Add', ['$scope', function($scope){
-	$scope.Address="Abcd";
-	
+	$scope.Address="786,Buckingham Palace,Old Trafford,North London.";
+	$scope.Age="24";
+	$scope.Email="Sameer@CR7.com";
+	$scope.Gender="Male";
+	$scope.DOB="17/04/1993";
+	$scope.BloodGroup=["A+ve","A-ve","B+ve","B-ve","O+ve","AB+ve","O-ve"];
+	$scope.Occupation="Software Engineer";
+}])
+app.controller('add1',['$scope',function  ($scope) {
+	$scope.Height="5.6ft";
+	$scope.weight="70kgs";
+	$scope.BMI="24.5";
+	$scope.Allergies="Pain Killers";
+	$scope.Mediclaim=["Yes","No"];
+	$scope.Mediclaim_ID="NBHC02758";
 }])
